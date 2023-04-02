21b7f51d7f8e3feb11cb603b35b399083cbca711
MahmoudAli24 <ememe50@yahoo.com>
2023-04-03 01:48:46 +0200

    HI

--- content/js/book.js
+++ content/js/book.js
@@ -4,3 +4,31 @@ let NavBarList = document.querySelector(".main-nav__list");
 menu.addEventListener("click", () => {
   NavBarList.classList.toggle("show");
 });
+
+// Start Overlay
+function validateForm() {
+  var cardNumber = document
+    .getElementById("card-number")
+    .value.replace(/\s+/g, "");
+  var cvv = document.getElementById("cvv").value;
+  if (!validateCardNumber(cardNumber)) {
+    alert("Invalid credit card number.");
+    return false;
+  }
+  if (!validateCVV(cvv)) {
+    alert("Invalid CVV.");
+    return false;
+  }
+  return true;
+}
+
+function validateCardNumber(cardNumber) {
+  var regex =
+    /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13})$/;
+  return regex.test(cardNumber);
+}
+
+function validateCVV(cvv) {
+  var regex = /^[0-9]{3}$/;
+  return regex.test(cvv);
+}
